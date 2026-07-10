89dcf5e3d2e94bb107421e6773d23bc43bc2797b
Ramyamv24 <ramyavani689@gmail.com>
2026-07-10 17:49:50 +0530

    Palindromic substrings

---
+++ Palindromic substrings.py
@@ -0,0 +1,26 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        count = 0
+
+        def expand(left, right):
+            res = 0
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                res += 1
+                left -= 1
+                right += 1
+            return res
+
+        for i in range(len(s)):
+            count += expand(i, i)      # Odd length palindrome
+            count += expand(i, i + 1)  # Even length palindrome
+
+        return count
+
+
+# Main part
+s = input("Enter a string: ")
+
+sol = Solution()
+result = sol.countSubstrings(s)
+
+print("Number of palindromic substrings:", result)
